5db18e19c5a0a88c62f740466e6729d264f0a4af
ajoy267 <andrew.joy12@hotmail.com>
2021-10-08 13:39:31 -0700

    created an event listener for the button to create a random number when the button is clicked

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // import functions and grab DOM elements
-
+const button = document.getElementById('generate');
+const resultMessage = document.getElementById('random-num');
 // initialize global state
-
+let randomNum = Math.ceil(Math.random() * 10000000);
 // set event listeners 
-// get user input
-// use user input to update state 
-// update DOM to reflect the new state
-
+button.addEventListener('click', () => {
+    resultMessage.textContent = randomNum;
+    randomNum = Math.ceil(Math.random() * 10000000);
+});
